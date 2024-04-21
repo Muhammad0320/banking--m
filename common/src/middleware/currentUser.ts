2133aef59b1b4f1f5296ccea80f5c933045cb5ac
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-21 16:28:00 +0100

    chore: update common dir

--- common/src/middleware/currentUser.ts
+++ common/src/middleware/currentUser.ts
@@ -1,8 +1,18 @@
 import { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
+import { UserStatus } from '../enums/UserStatus';
+import { UserRole } from '../enums/UserRoles';
 
 interface UserPayload {
   id: string;
+  name: string;
+  email: string;
+  password: string;
+  role: UserRole;
+  avatar: string;
+  createdAt: Date;
+  status: UserStatus;
+  passwordConfirm: string;
 }
 
 declare global {
