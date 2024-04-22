a835773f8c74b9ffbe283596bd550353522f5ae3
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-22 17:34:29 +0100

    chore:  update block ts

--- account/src/routes/block.ts
+++ account/src/routes/block.ts
@@ -1,12 +1,26 @@
-import { paramsChecker } from '@m0banking/common';
+import { BadRequest, NotFound, paramsChecker } from '@m0banking/common';
 import express, { Request, Response } from 'express';
+import Account from '../model/account';
+import { AccountStatus } from '../enums/AccountStatusEnum';
 
 const router = express.Router();
 
 router.patch(
   '/block/:id',
   paramsChecker('id'),
-  async (req: Request, res: Response) => {}
+  async (req: Request, res: Response) => {
+    const updatedAccount = await Account.findByIdAndUpdate(
+      req.params.id,
+      { status: AccountStatus.Blocked },
+      { new: true }
+    );
+
+    if (!updatedAccount) {
+      throw new NotFound('User not found');
+    }
+
+    res.status(200).json({ status: 'success', data: updatedAccount });
+  }
 );
 
 export { router as BlockUserRouter };
