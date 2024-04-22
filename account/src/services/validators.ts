e81e4259bd11ceff68e21179f96907911c8c9862
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-22 17:22:00 +0100

    chore:  update validator

--- account/src/services/validators.ts
+++ account/src/services/validators.ts
@@ -1,4 +1,7 @@
 import { body } from 'express-validator';
+import { AccountTier } from '../enums/AccountTier';
+import { AccountCurrency } from '../enums/AccountCurrencyEnum';
+import mongoose from 'mongoose';
 
 export const pinValidator = () =>
   body('pin')
@@ -12,3 +15,28 @@ export const pinConfirmValidator = () =>
       input === req.body.pin;
     })
     .withMessage('Pins should be the same');
+
+export const tierValidator = () =>
+  body('tier')
+    .trim()
+    .notEmpty()
+    .custom((input: string) =>
+      Object.values(AccountTier).includes(input as AccountTier)
+    )
+    .withMessage('please prove a valid tier');
+
+export const currencyValidator = () =>
+  body('currency')
+    .trim()
+    .notEmpty()
+    .custom((input: string) =>
+      Object.values(AccountCurrency).includes(input as AccountCurrency)
+    )
+    .withMessage('please provide a valid currency type');
+
+export const userIdValidator = () =>
+  body('userId')
+    .trim()
+    .notEmpty()
+    .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+    .withMessage('please provide a valid mongoose id');
