4284c8c3d05579e17c9c4428387fc61f649631bd
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-22 05:20:25 +0100

    chore:  validate inputs, userId and status

--- account/src/routes/new.ts
+++ account/src/routes/new.ts
@@ -1,5 +1,28 @@
-import express from 'express';
+import { requireAuth } from '@m0banking/common';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import mongoose from 'mongoose';
+import { AccountStatus } from '../enums/AccountStatusEnum';
 
 const router = express.Router();
+
+router.post(
+  '/',
+  requireAuth,
+  [
+    body('userId')
+      .trim()
+      .notEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage('please provide a valid mongoose id'),
+    body('status')
+      .trim()
+      .notEmpty()
+      .custom((input: string) =>
+        Object.values(AccountStatus).includes(input as AccountStatus)
+      )
+  ],
+  async (req: Request, rees: Response) => {}
+);
 
 export { router as createAccountRouter };
