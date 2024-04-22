5dfe210fd72ae5a13cdcdc446c07d1f14a901db0
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-22 05:43:21 +0100

    chore: update new ts

--- account/src/routes/new.ts
+++ account/src/routes/new.ts
@@ -3,6 +3,8 @@ import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import mongoose from 'mongoose';
 import { AccountStatus } from '../enums/AccountStatusEnum';
+import { AccountTier } from '../enums/AccountTier';
+import { AccountCurrency } from '../enums/AccountCurrencyEnum';
 
 const router = express.Router();
 
@@ -15,12 +17,25 @@ router.post(
       .notEmpty()
       .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
       .withMessage('please provide a valid mongoose id'),
-    body('status')
+    body('currency')
       .trim()
       .notEmpty()
       .custom((input: string) =>
-        Object.values(AccountStatus).includes(input as AccountStatus)
+        Object.values(AccountCurrency).includes(input as AccountCurrency)
       )
+      .withMessage('please provide a valid currency type'),
+
+    body('tier')
+      .trim()
+      .notEmpty()
+      .custom((input: string) =>
+        Object.values(AccountTier).includes(input as AccountTier)
+      )
+      .withMessage('please prove a valid tier'),
+
+    body('pin')
+      .isInt({ min: 4, max: 4 })
+      .withMessage('Account pin should be exactly 4 numbers')
   ],
   async (req: Request, rees: Response) => {}
 );
