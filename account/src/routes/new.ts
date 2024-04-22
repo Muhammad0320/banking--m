bd2c9fdac7955ce96ae473136e157c486255e03f
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-22 17:23:23 +0100

    refactor: the new requesst validator

--- account/src/routes/new.ts
+++ account/src/routes/new.ts
@@ -11,6 +11,13 @@ import {
 } from '@m0banking/common';
 import Account from '../model/account';
 import { AccountType } from '../enums/AccountTypeEnum';
+import {
+  currencyValidator,
+  pinConfirmValidator,
+  pinValidator,
+  tierValidator,
+  userIdValidator
+} from '../services/validators';
 
 const router = express.Router();
 
@@ -18,37 +25,11 @@ router.post(
   '/',
   requireAuth,
   [
-    body('userId')
-      .trim()
-      .notEmpty()
-      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
-      .withMessage('please provide a valid mongoose id'),
-    body('currency')
-      .trim()
-      .notEmpty()
-      .custom((input: string) =>
-        Object.values(AccountCurrency).includes(input as AccountCurrency)
-      )
-      .withMessage('please provide a valid currency type'),
-
-    body('tier')
-      .trim()
-      .notEmpty()
-      .custom((input: string) =>
-        Object.values(AccountTier).includes(input as AccountTier)
-      )
-      .withMessage('please prove a valid tier'),
-
-    body('pin')
-      .isInt({ min: 4, max: 4 })
-      .withMessage('Account pin should be exactly 4 numbers'),
-
-    body('pinConfirm')
-      .isInt({ min: 4, max: 4 })
-      .custom((input: number, { req }) => {
-        input === req.body.pin;
-      })
-      .withMessage('Pins should be the same')
+    pinValidator(),
+    pinConfirmValidator(),
+    tierValidator(),
+    currencyValidator(),
+    userIdValidator()
   ],
   requestValidator,
 
