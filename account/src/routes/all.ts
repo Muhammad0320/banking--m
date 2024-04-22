98865dd241ddf5592cc28d1cbe692ccdc009a1c9
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-22 12:51:04 +0100

    chore: update gel all accounts route

--- account/src/routes/all.ts
+++ account/src/routes/all.ts
@@ -1,5 +1,18 @@
-import express from 'express';
+import { accessibleTo, requireAuth, UserRole } from '@m0banking/common';
+import express, { Request, Response } from 'express';
+import Account from '../model/account';
 
 const router = express.Router();
+
+router.get(
+  '/',
+  requireAuth,
+  accessibleTo(UserRole.Admin),
+  async (req: Request, res: Response) => {
+    const allAccounts = await Account.find({});
+
+    res.status(200).json({ status: 'success', data: allAccounts });
+  }
+);
 
 export { router as allAccountsRouter };
