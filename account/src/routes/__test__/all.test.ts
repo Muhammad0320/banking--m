fafd31f46584f059e5adff0078b77c1e12ce7a14
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-23 18:13:40 +0100

    chore:  add test that returns a 403 for not admin users

--- account/src/routes/__test__/all.test.ts
+++ account/src/routes/__test__/all.test.ts
@@ -1,5 +1,7 @@
 import request from 'supertest';
 import { app } from '../../app';
+import { UserRole } from '@m0banking/common';
+import mongoose from 'mongoose';
 
 it('returns error other than 404, if the route is available', async () => {
   const { statusCode } = await request(app)
@@ -16,4 +18,29 @@ it('returns a 401, if unauthorized user access', async () => {
 
     .send()
     .expect(401);
+});
+
+it('returns a 403 for not admin users', async () => {
+  await request(app)
+    .get('/api/v1/account')
+    .set(
+      'Cookie',
+      await global.signin(new mongoose.Types.ObjectId().toHexString())
+    )
+    .send()
+    .expect(403);
+});
+
+it('returns a 200 for admin', async () => {
+  await request(app)
+    .get('/api/v1/account')
+    .set(
+      'Cookie',
+      await global.signin(
+        new mongoose.Types.ObjectId().toHexString(),
+        UserRole.Admin
+      )
+    )
+    .send()
+    .expect(200);
 });
