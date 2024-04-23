0797b1eb45a721db37fb059df3c55442ccccda17
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-23 05:18:32 +0100

    chore: add test that returns a 201, for valid inputs

--- account/src/routes/__test__/new.test.ts
+++ account/src/routes/__test__/new.test.ts
@@ -4,6 +4,8 @@ import { UserRole, UserStatus } from '@m0banking/common';
 import mongoose from 'mongoose';
 import { AccountCurrency } from '../../enums/AccountCurrencyEnum';
 import { AccountTier } from '../../enums/AccountTier';
+import Account from '../../model/account';
+import { response } from 'express';
 
 it('returns a error other that 404 if the route exists', async () => {
   const response = await request(app)
@@ -64,4 +66,25 @@ it('returns a 401, for unautheticated user', async () => {
       pinConfirm: '123456'
     })
     .expect(400);
+});
+
+it('returns a 201, for valid inputs', async () => {
+  const responseBody1 = await Account.find();
+
+  expect(responseBody1.length).toEqual(0);
+
+  await request(app)
+    .post('/api/v1/account')
+    .set('Cookie', await global.signin())
+    .send({
+      currency: AccountCurrency.NGN,
+      tier: AccountTier.Basic,
+      pin: '1234',
+      pinConfirm: '1234'
+    })
+    .expect(201);
+
+  const responseBody2 = await Account.find();
+
+  expect(responseBody2.length).toEqual(1);
 });
