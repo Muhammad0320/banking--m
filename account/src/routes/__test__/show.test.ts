092d316e97923a0f89a3ca2ed3cc4f5d2a2b4506
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-23 18:28:42 +0100

    chore: ad dtest that returns a 400, if user passes invalid mongoose id

--- account/src/routes/__test__/show.test.ts
+++ account/src/routes/__test__/show.test.ts
@@ -21,3 +21,11 @@ it('returns a 400, if a user w/ such id does not exist', async () => {
     .send()
     .expect(400);
 });
+
+it('returns a 400, if user passes invalid mongodb id', async () => {
+  await request(app)
+    .get('/api/v1/account/' + 'accountId')
+    .set('Cookie', await global.signin())
+    .send()
+    .expect(400);
+});
