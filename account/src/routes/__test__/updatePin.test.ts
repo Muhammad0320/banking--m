71335cf02cb119d866a0e57aa77c727a2cd19c96
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-23 19:05:29 +0100

    chore:  uadd tes tthat returns a 404 on invalid mongoose id

--- account/src/routes/__test__/updatePin.test.ts
+++ account/src/routes/__test__/updatePin.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import { app } from '../../app';
+import mongoose from 'mongoose';
 
 it('returns a 400 on invalid  mongoose id', async () => {
   await request(app)
@@ -7,4 +8,14 @@ it('returns a 400 on invalid  mongoose id', async () => {
     .set('Cookie', await global.signin())
     .send()
     .expect(400);
+});
+
+it('returns a 404 on invalid mongoose id', async () => {
+  const accountId = new mongoose.Types.ObjectId().toHexString();
+
+  await request(app)
+    .patch('/api/v1/account/updatePin/' + accountId)
+    .set('Cookie', await global.signin())
+    .send()
+    .expect(404);
 });
