c33de1ccca29268e5671ca58282a9625bdd6d015
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-23 05:20:09 +0100

    chore:  updte returns a 401, for unautheticated user

--- account/src/routes/__test__/new.test.ts
+++ account/src/routes/__test__/new.test.ts
@@ -58,14 +58,13 @@ it('returns a 400, if pins are longer that 4', async () => {
 it('returns a 401, for unautheticated user', async () => {
   await request(app)
     .post('/api/v1/account')
-
     .send({
       currency: AccountCurrency.NGN,
       tier: AccountTier.Basic,
-      pin: '123456',
-      pinConfirm: '123456'
+      pin: '1234',
+      pinConfirm: '1234'
     })
-    .expect(400);
+    .expect(401);
 });
 
 it('returns a 201, for valid inputs', async () => {
