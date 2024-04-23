478cf5afb29f4f5bc83c15160ffa9c920fe80f07
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-23 18:24:15 +0100

    chore: add tes that returns a 401 on unauthorized user access

--- account/src/routes/__test__/show.test.ts
+++ account/src/routes/__test__/show.test.ts
@@ -2,6 +2,16 @@ import request from 'supertest';
 import { app } from '../../app';
 import mongoose from 'mongoose';
 
+it('returns a 401 on unauthorized user access', async () => {
+  const accountId = new mongoose.Types.ObjectId().toHexString();
+
+  await request(app)
+    .get('/api/v1/account/' + accountId)
+
+    .send()
+    .expect(401);
+});
+
 it('returns a 400, if a user w/ such id does not exist', async () => {
   const accountId = new mongoose.Types.ObjectId().toHexString();
 
