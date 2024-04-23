d6c84c44e2f3a59a3f77ba234a5d68e7c8b078de
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-23 17:28:40 +0100

    chore: add all test

--- account/src/routes/__test__/all.test.ts
+++ account/src/routes/__test__/all.test.ts
@@ -9,3 +9,11 @@ it('returns error other than 404, if the route is available', async () => {
 
   expect(statusCode).not.toEqual(404);
 });
+
+it('returns a 401, if unauthorized user access', async () => {
+  await request(app)
+    .get('/api/v1/account')
+
+    .send()
+    .expect(401);
+});
