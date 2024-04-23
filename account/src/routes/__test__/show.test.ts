07152ff52c1b863d518466f771cceb9891959bc1
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-23 18:40:20 +0100

    chore: add test that returns a 200, if user is an admin

--- account/src/routes/__test__/show.test.ts
+++ account/src/routes/__test__/show.test.ts
@@ -3,6 +3,7 @@ import { app } from '../../app';
 import mongoose from 'mongoose';
 import { AccountCurrency } from '../../enums/AccountCurrencyEnum';
 import { AccountTier } from '../../enums/AccountTier';
+import { UserRole } from '@m0banking/common';
 
 it('returns a 401 on unauthorized user access', async () => {
   const accountId = new mongoose.Types.ObjectId().toHexString();
@@ -57,4 +58,34 @@ it('returns a 403, if user tried to check other users account', async () => {
     )
     .send()
     .expect(403);
+});
+
+it('returns a 200, if user is an admin', async () => {
+  const {
+    body: { data }
+  } = await request(app)
+    .post('/api/v1/account')
+    .set(
+      'Cookie',
+      await global.signin(new mongoose.Types.ObjectId().toHexString())
+    )
+    .send({
+      currency: AccountCurrency.NGN,
+      tier: AccountTier.Basic,
+      pin: 1234,
+      pinConfirm: 1234
+    })
+    .expect(201);
+
+  await request(app)
+    .get('/api/v1/account/' + data.id)
+    .set(
+      'Cookie',
+      await global.signin(
+        new mongoose.Types.ObjectId().toHexString(),
+        UserRole.Admin
+      )
+    )
+    .send()
+    .expect(200);
 });
