10c31f68604bbfdb8f996881159b88e44be0db3a
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-24 05:21:45 +0100

    chore: update block test ts

--- account/src/routes/__test__/block.test.ts
+++ account/src/routes/__test__/block.test.ts
@@ -2,6 +2,8 @@ import request from 'supertest';
 import { app } from '../../app';
 import mongoose from 'mongoose';
 import { UserRole } from '@m0banking/common';
+import { AccountCurrency } from '../../enums/AccountCurrencyEnum';
+import { AccountTier } from '../../enums/AccountTier';
 
 it('returns a 400  for invalid mongoose  id', async () => {
   await request(app)
@@ -45,4 +47,31 @@ it('returns a 404, on invalid id', async () => {
     )
     .send()
     .expect(404);
+});
+
+it('returns a 200, when everything is valid', async () => {
+  const {
+    body: { data }
+  } = await request(app)
+    .post('/api/v1/account')
+    .set('Cookie', await global.signin())
+    .send({
+      currency: AccountCurrency.NGN,
+      tier: AccountTier.Basic,
+      pin: 1234,
+      pinConfirm: 1234
+    })
+    .expect(201);
+
+  await request(app)
+    .post('/api/v1/account/block/' + data.id)
+    .set(
+      'Cookie',
+      await global.signin(
+        new mongoose.Types.ObjectId().toHexString(),
+        UserRole.CustomerService
+      )
+    )
+    .send()
+    .expect(200);
 });
