62bef9bf3075631c1f26faec338f637c2c8ff361
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-24 05:29:53 +0100

    chore: update unblock ts

--- account/src/routes/__test__/unblock.test.ts
+++ account/src/routes/__test__/unblock.test.ts
@@ -12,3 +12,13 @@ it('returns a 400  for invalid mongoose  id', async () => {
     .send()
     .expect(400);
 });
+
+it('returns a 401, for unauthorized user', async () => {
+  await request(app)
+    .post(
+      '/api/v1/account/block/' + new mongoose.Types.ObjectId().toHexString()
+    )
+
+    .send()
+    .expect(401);
+});
