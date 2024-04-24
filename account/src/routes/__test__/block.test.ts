b0cdb33388a0863727ab42b717d7efdf6cbdd1c7
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-24 05:09:20 +0100

    test: add test that returns a 401, for unauthorized user

--- account/src/routes/__test__/block.test.ts
+++ account/src/routes/__test__/block.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import { app } from '../../app';
+import mongoose from 'mongoose';
 
 it('returns a 400  for invalid mongoose  id', async () => {
   await request(app)
@@ -7,4 +8,14 @@ it('returns a 400  for invalid mongoose  id', async () => {
     .set('Cookie', await global.signin())
     .send()
     .expect(400);
+});
+
+it('returns a 401, for unauthorized user', async () => {
+  await request(app)
+    .post(
+      '/api/v1/account/block/' + new mongoose.Types.ObjectId().toHexString()
+    )
+
+    .send()
+    .expect(401);
 });
