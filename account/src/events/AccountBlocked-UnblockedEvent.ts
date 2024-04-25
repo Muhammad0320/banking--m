9dfa16b4d3f170e1a305e50c85a95ef724e5750e
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-25 19:44:37 +0100

    chore: update Accunt blocked evebt

--- account/src/events/AccountBlocked-UnblockedEvent.ts
+++ account/src/events/AccountBlocked-UnblockedEvent.ts
@@ -1,1 +1,13 @@
-export interface AccountBlocked_UnblockedEvent {}
+import { Subjects } from '@m0banking/common';
+
+export interface AccountBlockedEvent {
+  subject: Subjects.AccountBlocked;
+
+  data: {
+    id: string;
+    version: string;
+    user: {
+      id: string;
+    };
+  };
+}
