0735347bb9a4c6e74e0b89159cbb51542e519b61
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-25 04:55:45 +0100

    chore: update publisher ts

--- common/src/events/Publisher.ts
+++ common/src/events/Publisher.ts
@@ -1,1 +1,25 @@
-interface Event {}
+import { Stan } from "node-nats-streaming";
+import { Subjects } from "./Subjects";
+
+interface Event {
+  subject: Subjects;
+  data: any;
+}
+
+export abstract class Publisher<T extends Event> {
+  abstract subject: T["subject"];
+
+  constructor(protected client: Stan) {}
+
+  publish(data: Event["data"]): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.client.publish(this.subject, JSON.stringify(data), (error) => {
+        if (error) {
+          return reject(error);
+        }
+      });
+
+      return resolve();
+    });
+  }
+}
