2543fa6a058b23e504104adc028917c0d6dff0aa
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-26 06:48:51 +0100

    chore: update common dir

--- common/src/events/Publisher.ts
+++ common/src/events/Publisher.ts
@@ -11,7 +11,7 @@ export abstract class Publisher<T extends Event> {
 
   constructor(protected client: Stan) {}
 
-  publish(data: Event["data"]): Promise<void> {
+  publish(data: T["data"]): Promise<void> {
     return new Promise((resolve, reject) => {
       this.client.publish(this.subject, JSON.stringify(data), (error) => {
         if (error) {
