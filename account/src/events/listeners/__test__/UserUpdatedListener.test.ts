6642b0899691bec6f3400266185d534cb040d56c
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-26 19:44:19 +0100

    test: update set up func

--- account/src/events/listeners/__test__/UserUpdatedListener.test.ts
+++ account/src/events/listeners/__test__/UserUpdatedListener.test.ts
@@ -1,1 +1,33 @@
-const setup = async () => {};
+import mongoose from 'mongoose';
+import { User } from '../../../model/user';
+import { natsWrapper } from '../../../natswrapper';
+import { UserUpdatedListener } from '../UserUpdatedListener';
+import { UserRole, UserUpdatedEvent } from '@m0banking/common';
+
+const setup = async () => {
+  // create a listener
+  const listener = new UserUpdatedListener(natsWrapper.client);
+
+  // create a user
+
+  const newUSer = await User.buildUser({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    name: 'Lisan al-gaib',
+    email: 'lisanalgaib@gmail.com',
+    password: 'shit passwor',
+    version: 0,
+    role: UserRole.User
+  });
+
+  // create a data
+
+  const data: UserUpdatedEvent['data'] = {
+    email: newUSer.email,
+    name: 'Al mehdi usul',
+    password: newUSer.password,
+    id: newUSer.id,
+    version: newUSer.version + 1
+  };
+
+  // create msg object
+};
