5f75b91e8a05b9a278028414ba0242e3d5738879
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-26 18:42:09 +0100

    chore: updated user updated listener

--- account/src/events/listeners/UserUpdatedListener.ts
+++ account/src/events/listeners/UserUpdatedListener.ts
@@ -1,1 +1,23 @@
-export class UserUpdatedListener {}
+import {
+  Listener,
+  NotFound,
+  Subjects,
+  UserUpdatedEvent
+} from '@m0banking/common';
+import { queueGroupName } from './queueGroupName';
+import { Message } from 'node-nats-streaming';
+import { User } from '../../model/user';
+
+export class UserUpdatedListener extends Listener<UserUpdatedEvent> {
+  readonly subject = Subjects.UserUpdated;
+
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: UserUpdatedEvent['data'], msg: Message) {
+    const user = await User.findByLastVersionNumberAndId(data.id, data.version);
+
+    if (!user) throw new NotFound('There is no user w/ such id and version');
+
+    msg.ack();
+  }
+}
