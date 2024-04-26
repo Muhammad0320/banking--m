4409a17e2a3b465051677e03f4d355cbe2accd15
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-26 05:35:22 +0100

    chore:  update account unblocked

--- account/src/events/publishers/AccountUnblockedPublisher.ts
+++ account/src/events/publishers/AccountUnblockedPublisher.ts
@@ -1,1 +1,7 @@
-export class AccountUnblockedPublisher {}
+import { AccountUnBlockedEvent, Publisher, Subjects } from '@m0banking/common';
+
+export class AccountUnblockedPublisher extends Publisher<
+  AccountUnBlockedEvent
+> {
+  readonly subject = Subjects.AccountUnblocked;
+}
