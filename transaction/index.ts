cdfdd5337c9a8205e776c0e4b3420b9e9fb736aa
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-27 04:53:00 +0100

    chore: add transaction serv

---
+++ transaction/index.ts
@@ -0,0 +1,58 @@
+import mongoose from 'mongoose';
+import { app } from './app';
+import { natsWrapper } from './natswrapper';
+import { UserCreatedLitener } from './events/listeners/UserCreatedListener';
+import { UserUpdatedListener } from './events/listeners/UserUpdatedListener';
+
+const start = async () => {
+  const port = 3000;
+
+  if (!process.env.JWT_KEY) {
+    throw new Error(' JWT_KEY not found ');
+  }
+
+  if (!process.env.MONGO_URI) {
+    throw new Error(' MONGO_URI not found ');
+  }
+
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS_CLIENT_ID must be defined');
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS_URL must be defined');
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID must be defined');
+  }
+
+  try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+
+    natsWrapper.client.on('close', () => {
+      console.log(' NATS connection closed! ');
+      process.exit();
+    });
+
+    process.on('SIGTERM', () => natsWrapper.client.close());
+    process.on('SIGINT', () => natsWrapper.client.close());
+
+    new UserCreatedLitener(natsWrapper.client).listen();
+    new UserUpdatedListener(natsWrapper.client).listen();
+
+    await mongoose.connect(process.env.MONGO_URI);
+
+    console.log('connected to mongoDB');
+  } catch (error) {
+    console.error(error);
+  }
+
+  app.listen(port, () => {
+    console.log('App running on port ', port);
+  });
+};
+
+start();
