a361a41b5683fb1f26657453d7a16cb79fd7a26a
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-27 15:01:03 +0100

    chore:  update account created listenner

--- transaction/src/events/listener/AccountCreatedListener.ts
+++ transaction/src/events/listener/AccountCreatedListener.ts
@@ -1,1 +1,16 @@
-export class AccountCreatedListener {}
+import { AccountCreatedEvent, Listener, Subjects } from '@m0banking/common';
+import { queueGroupName } from './queueGroupName';
+import { Message } from 'node-nats-streaming';
+import { Account } from '../../model/account';
+
+export class AccountCreatedListener extends Listener<AccountCreatedEvent> {
+  readonly subject = Subjects.AccountCreated;
+
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: AccountCreatedEvent['data'], msg: Message) {
+    const pin = '1967';
+
+    await Account.buildAccount({ ...data, pin });
+  }
+}
