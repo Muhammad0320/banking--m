56c61a71975bcd0a0cfaadf5f4cbcf054a5befa4
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-27 16:05:08 +0100

    chore: kludged

--- transaction/src/model/transaction.ts
+++ transaction/src/model/transaction.ts
@@ -3,12 +3,30 @@ import { AccountDoc } from './account';
 
 type TxnAttrs = {
   id: string;
-  accountId: AccountDoc;
+  account: AccountDoc;
   amount: number;
 };
 
-type TxnDoc = mongoose.Document & TxnAttrs & { version: number };
+type TxnDoc = mongoose.Document &
+  TxnAttrs & { version: number; createdAt: Date };
 
 type TxnModel = mongoose.Model<TxnDoc> & {
   buildTxn(attrs: TxnAttrs): TxnDoc;
 };
+
+const txnSchema = new mongoose.Schema<TxnDoc, TxnModel>({
+  account: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Account'
+  },
+
+  amount: {
+    type: Number,
+    required: true
+  },
+
+  createdAt: {
+    type: mongoose.Schema.Types.Date,
+    default: new Date()
+  }
+});
