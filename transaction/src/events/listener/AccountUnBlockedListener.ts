a7da28723d1016f6444f2373ac34dc73b6b0a9c3
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-27 15:40:47 +0100

    chore: update accoutn unblocked litenenr

--- transaction/src/events/listener/AccountUnBlockedListener.ts
+++ transaction/src/events/listener/AccountUnBlockedListener.ts
@@ -1,1 +1,28 @@
-export class AccountUnblocked {}
+import {
+  AccountStatus,
+  AccountUnBlockedEvent,
+  Listener,
+  Subjects
+} from '@m0banking/common';
+import { queueGroupName } from './queueGroupName';
+import { Message } from 'node-nats-streaming';
+import { Account } from '../../model/account';
+
+export class AccountUnblocked extends Listener<AccountUnBlockedEvent> {
+  readonly subject = Subjects.AccountUnblocked;
+
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: AccountUnBlockedEvent['data'], msg: Message) {
+    const account = await Account.findByLastVersionAndId(data.id, data.version);
+
+    if (!account) {
+      throw new Error('Add account unblockd error');
+    }
+
+    account.set({ status: AccountStatus.Active, _block: false });
+    await account.save();
+
+    msg.ack();
+  }
+}
