9656ac5272bdacac47520cd1f8a1f233261a9cc8
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-27 14:45:30 +0100

    chore:  update account ts

--- transaction/src/model/account.ts
+++ transaction/src/model/account.ts
@@ -1,5 +1,6 @@
 import { AccountCurrency, AccountStatus } from '@m0banking/common';
 import mongoose from 'mongoose';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
 
 type AccountAttrs = {
   id: string;
@@ -20,37 +21,51 @@ type AccountModel = mongoose.Model<AccountDoc> & {
   buildAccount(attrs: AccountAttrs): Promise<AccountDoc>;
 };
 
-const AccountSchema = new mongoose.Schema<AccountDoc, AccountModel>({
-  no: {
-    type: Number,
-    unique: true
-  },
+const accountSchema = new mongoose.Schema(
+  {
+    no: {
+      type: Number,
+      unique: true
+    },
 
-  userId: {
-    type: String,
-    unique: true,
-    required: true
-  },
+    userId: {
+      type: String,
+      unique: true,
+      required: true
+    },
 
-  currency: {
-    type: String,
-    enum: Object.values(AccountCurrency),
-    default: AccountCurrency.USD
-  },
+    currency: {
+      type: String,
+      enum: Object.values(AccountCurrency),
+      default: AccountCurrency.USD
+    },
 
-  status: {
-    type: String,
-    enum: Object.values(AccountStatus),
-    default: AccountStatus.Active
-  },
+    status: {
+      type: String,
+      enum: Object.values(AccountStatus),
+      default: AccountStatus.Active
+    },
 
-  _block: {
-    type: Boolean,
-    default: false
-  },
+    _block: {
+      type: Boolean,
+      default: false
+    },
 
-  balance: {
-    type: Number,
-    default: 0
+    balance: {
+      type: Number,
+      default: 0
+    }
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+
+        delete ret._id;
+      }
+    }
   }
-});
+);
+
+accountSchema.set('versionKey', 'version');
+accountSchema.plugin(updateIfCurrentPlugin);
