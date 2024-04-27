99fc562aaa7661098bd734a01a854f5175d76f6a
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-27 15:33:14 +0100

    chore: update account blocked ts

--- transaction/src/events/listener/AccountBlockedListener.ts
+++ transaction/src/events/listener/AccountBlockedListener.ts
@@ -1,11 +1,28 @@
-import { AccountBlockedEvent, Listener, Subjects } from '@m0banking/common';
+import {
+  AccountBlockedEvent,
+  AccountStatus,
+  Listener,
+  Subjects
+} from '@m0banking/common';
 import { queueGroupName } from './queueGroupName';
 import { Message } from 'node-nats-streaming';
+import { Account } from '../../model/account';
 
 export class AccountBlockedListener extends Listener<AccountBlockedEvent> {
   readonly subject = Subjects.AccountBlocked;
 
   queueGroupName = queueGroupName;
 
-  async onMessage(data: AccountBlockedEvent['data'], msg: Message) {}
+  async onMessage(data: AccountBlockedEvent['data'], msg: Message) {
+    const account = await Account.findByLastVersionAndId(data.id, data.version);
+
+    if (!account) {
+      throw new Error('Account not found');
+    }
+
+    account.set({ status: AccountStatus.Blocked, _block: true });
+    await account.save();
+
+    msg.ack();
+  }
 }
