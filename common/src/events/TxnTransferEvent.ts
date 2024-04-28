34d3c16bae660da14329e31e5761a4f508ad0359
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-28 09:02:24 +0100

    chore:  update the txn transfer event

---
+++ common/src/events/TxnTransferEvent.ts
@@ -0,0 +1,15 @@
+import { Subjects } from "./Subjects";
+
+export interface TxnTransferCreatedEvent {
+  subject: Subjects.TxnTransferCreated;
+
+  data: {
+    id: string;
+    version: number;
+    account: {
+      id: string;
+      version: number;
+      balance: number;
+    };
+  };
+}
