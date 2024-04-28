bfc18c4219942f5a02c704265e180e7fb673f3b4
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-28 07:12:25 +0100

    chore: update update tansaction

--- transaction/src/routes/newTransfer.ts
+++ transaction/src/routes/newTransfer.ts
@@ -1,8 +1,12 @@
-import { requestValidator, requireAuth } from '@m0banking/common';
+import { NotFound, requestValidator, requireAuth } from '@m0banking/common';
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import mongoose from 'mongoose';
 import { validateAccount } from '../middlewares/validateAccount';
+import { Account } from '../model/account';
+import { Txn } from '../model/transaction';
+import { TxnTypeEnum } from '../enums/TxnTypeEnum';
+import { TxnStatusEnum } from '../enums/TxnStatusEnum';
 
 const router = express.Router();
 
@@ -21,7 +25,29 @@ router.get(
   ],
   requestValidator,
   validateAccount('transfer'),
-  async (req: Request, response: Response) => {}
+  async (req: Request, res: Response) => {
+    const { amount, accountId, beneficiaryId } = req.body;
+
+    const account = await Account.findById(accountId);
+
+    const beneficiary = await Account.findById(beneficiaryId);
+
+    if (!account || !beneficiary) throw new NotFound('');
+
+    await account!.updateOne({ balance: account!.balance - amount });
+
+    await beneficiary!.updateOne({ balance: beneficiary!.balance + +amount });
+
+    const newTransfer = await Txn.buildTxn({
+      amount,
+      beneficiary,
+      account,
+      status: TxnStatusEnum.Success,
+      type: TxnTypeEnum.Transfer
+    });
+
+    res.status(201).json({ status: 'success', data: newTransfer });
+  }
 );
 
 export { router as crreateWithdrawalRouter };
