98e57a34fb2d6fd187bc5d9d083ce030ca9f4618
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-28 15:19:32 +0100

    chore:  add test that automatically incrememnts version number

--- account/src/model/__test__/account.test.ts
+++ account/src/model/__test__/account.test.ts
@@ -40,3 +40,39 @@ it('implements occ', async () => {
 
   throw new Error('You coude must not reach this point');
 });
+
+it('automatically incrememnts version number', async () => {
+  const user = await User.buildUser({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    email: 'shit@gmail.com',
+    name: 'Shit man',
+    password: 'hfvfvvfbvfivfbi',
+    version: 0,
+    role: UserRole.User
+  });
+
+  const account = await Account.buildAccount({
+    currency: AccountCurrency.NGN,
+    tier: AccountTier.Basic,
+    pin: '1234',
+    pinConfirm: '1234',
+    user,
+    type: AccountType.Current
+  });
+
+  expect(account.version).toEqual(0);
+
+  const updatedAccount = account.set({ name: 'Carl' });
+
+  await updatedAccount.save();
+
+  expect(account.version).toEqual(1);
+
+  await updatedAccount.save();
+
+  expect(account.version).toEqual(2);
+
+  await updatedAccount.save();
+
+  expect(account.version).toEqual(3);
+});
