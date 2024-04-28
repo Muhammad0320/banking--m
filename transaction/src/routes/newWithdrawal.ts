d8ba86e84e4a07e286cab90358c2258589f50cc8
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-28 07:31:28 +0100

    chore:  update new withdrawal

--- transaction/src/routes/newWithdrawal.ts
+++ transaction/src/routes/newWithdrawal.ts
@@ -1,8 +1,23 @@
 import { requireAuth } from '@m0banking/common';
 import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import mongoose from 'mongoose';
 
 const router = express.Router();
 
-router.post('/', requireAuth, async (req: Request, res: Response) => {});
+router.post(
+  '/withdrwal',
+  requireAuth,
+  [
+    body('amount')
+      .isFloat({ gt: 0 })
+      .withMessage('Please enter a valid amount!'),
+    body('accountId')
+      .isString()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage('Please provide a valid accountId')
+  ],
+  async (req: Request, res: Response) => {}
+);
 
 export { router as newWithdrawalRouter };
