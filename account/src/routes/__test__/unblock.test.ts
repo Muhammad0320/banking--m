6c43c3a697b5fcaa8fd5b167842891218ed745eb
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-28 15:32:31 +0100

    chore: published unblok ts

--- account/src/routes/__test__/unblock.test.ts
+++ account/src/routes/__test__/unblock.test.ts
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import { AccountTier } from '../../enums/AccountTier';
 import { UserRole, AccountCurrency, AccountStatus } from '@m0banking/common';
 import { User } from '../../model/user';
+import { natsWrapper } from '../../natswrapper';
 
 it('returns a 400  for invalid mongoose  id', async () => {
   await request(app)
@@ -87,4 +88,46 @@ it('returns a 200, when everything is valid', async () => {
     .expect(200);
 
   expect(data2.status).toEqual(AccountStatus.Active);
+});
+
+it('returns a 200, when everything is valid', async () => {
+  const user = await User.buildUser({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    email: 'lisanalgaib@gmail.com',
+    name: 'Shit man',
+    password: 'shit-password',
+    role: UserRole.User,
+    version: 0
+  });
+
+  const {
+    body: { data }
+  } = await request(app)
+    .post('/api/v1/account')
+    .set('Cookie', await global.signin(user.id))
+    .send({
+      currency: AccountCurrency.NGN,
+      tier: AccountTier.Basic,
+      pin: 1234,
+      pinConfirm: 1234
+    })
+    .expect(201);
+
+  const {
+    body: { data: data2 }
+  } = await request(app)
+    .patch('/api/v1/account/unblock/' + data.id)
+    .set(
+      'Cookie',
+      await global.signin(
+        new mongoose.Types.ObjectId().toHexString(),
+        UserRole.CustomerService
+      )
+    )
+    .send()
+    .expect(200);
+
+  expect(data2.status).toEqual(AccountStatus.Active);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
