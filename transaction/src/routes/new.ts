3cacfbd87b11738e69bdc42040ecdf4a594e032b
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-28 04:49:45 +0100

    chore: validate inputs

--- transaction/src/routes/new.ts
+++ transaction/src/routes/new.ts
@@ -1,7 +1,22 @@
 import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import mongoose from 'mongoose';
 
 const router = express.Router();
 
-router.get('/', (req: Request, res: Response) => {});
+router.post(
+  '/',
+  [
+    body('amount').isFloat({ gt: 0 }),
+    body('accountId')
+      .isString()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input)),
+
+    body('beneficiaryId')
+      .isString()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+  ],
+  (req: Request, res: Response) => {}
+);
 
 export { router as createTxnRouter };
