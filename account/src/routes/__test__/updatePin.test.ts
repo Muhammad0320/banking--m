4dfd5673cc1621a5f3611793b1d541e3043d7b95
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-28 15:51:49 +0100

    chore:  add test that returns a 403, if another user tries to update pin

--- account/src/routes/__test__/updatePin.test.ts
+++ account/src/routes/__test__/updatePin.test.ts
@@ -108,13 +108,20 @@ it('returns a 400 on invalid input: pinConfirm ', async () => {
 });
 
 it('returns a 403, if another user tries to update pin', async () => {
-  const userId = new mongoose.Types.ObjectId().toHexString();
+  const user = await User.buildUser({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    email: 'lisanalgaib@gmail.com',
+    name: 'Shit man',
+    password: 'shit-password',
+    role: UserRole.User,
+    version: 0
+  });
 
   const {
     body: { data }
   } = await request(app)
     .post('/api/v1/account')
-    .set('Cookie', await global.signin(userId))
+    .set('Cookie', await global.signin(user.id))
     .send({
       currency: AccountCurrency.NGN,
       tier: AccountTier.Basic,
