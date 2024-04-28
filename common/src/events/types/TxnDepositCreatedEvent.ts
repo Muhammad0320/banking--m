38898b9d88a4645dd1c986e5b79e4d41205691cf
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-28 08:55:01 +0100

    chore: update txn deposit creeated

---
+++ common/src/events/types/TxnDepositCreatedEvent.ts
@@ -0,0 +1,15 @@
+import { Subjects } from "../Subjects";
+
+export interface TxnDepositCreatedEvent {
+  subject: Subjects.TxnDepositCreated;
+
+  data: {
+    id: string;
+    version: number;
+    account: {
+      id: string;
+      version: number;
+      balance: number;
+    };
+  };
+}
