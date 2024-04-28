ef06c0c805dec13f5f02750354afe83061d57dc6
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-28 05:35:11 +0100

    chore: update the middlwware that validates request

--- transaction/src/middlewares/validateAccount.ts
+++ transaction/src/middlewares/validateAccount.ts
@@ -1,7 +1,43 @@
 import { NextFunction, Request, Response } from 'express';
+import { Account } from '../model/account';
+import {
+  AccountStatus,
+  BadRequest,
+  Forbidden,
+  NotFound
+} from '@m0banking/common';
 
-export const validateRequest = (
+export const validateRequest = (type?: string) => async (
   req: Request,
   res: Response,
   next: NextFunction
-) => {};
+) => {
+  const { amount, accountId } = req.body;
+
+  const account = await Account.findById(accountId);
+
+  if (!account) throw new NotFound('Account not found');
+
+  if (account.status === AccountStatus.Blocked)
+    throw new Forbidden('This account is blocked');
+
+  if (type === 'withdrawal') {
+    account.balance >= amount && new BadRequest('Insufficient fund');
+
+    next();
+  }
+
+  if (type === 'transfer') {
+    account.balance >= amount && new BadRequest('Insufficient fund');
+
+    const beneficiaryAccount = await Account.findById(req.body.beneficiaryId);
+
+    if (!beneficiaryAccount)
+      throw new NotFound('Benebeneficiary Account not found');
+
+    if (beneficiaryAccount.status === AccountStatus.Blocked)
+      throw new Forbidden('This account is blocked');
+  }
+
+  next();
+};
