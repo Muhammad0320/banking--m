6141a2bf79de03515c0cb67996f3d6409d3fac51
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-28 05:43:58 +0100

    chore:  update app.ts

--- transaction/app.ts
+++ transaction/app.ts
@@ -3,6 +3,7 @@ import 'express-async-errors';
 import cookieSession from 'cookie-session';
 
 import { currentUser, globalErrorHandler, NotFound } from '@m0banking/common';
+import { createTxnRouter } from './src/routes/newDeposit';
 
 const app = express();
 
@@ -22,9 +23,11 @@ app.use(
 
 console.log('Hi mom');
 
-const rootUrl = '/api/v1/transaction';
+const rootUrl = '/api/v1/txn';
 
 app.use(currentUser);
+
+app.use(rootUrl, createTxnRouter);
 
 app.all('*', () => {
   throw new NotFound('Route not found');
