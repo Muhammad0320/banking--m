dca448ae1ef5d81f145bf49995f24707611ac012
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-28 14:33:17 +0100

    chore: update txan withdrawal publisher

--- transaction/src/events/publisher/TxnWithdrawalPublishert.ts
+++ transaction/src/events/publisher/TxnWithdrawalPublishert.ts
@@ -1,1 +1,11 @@
-class TxnWithdrawalPublisher {}
+import {
+  Publisher,
+  Subjects,
+  TxnWithdrawalCreatedEvent
+} from '@m0banking/common';
+
+export class TxnWithdrawalPublisher extends Publisher<
+  TxnWithdrawalCreatedEvent
+> {
+  readonly subject = Subjects.TxnWithdrawalCreated;
+}
