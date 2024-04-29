f3a5f0ad4a825266da0f91716ad1f10f144555fb
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-29 06:45:27 +0100

    chore:  update invalid attemptstracker

--- transaction/src/middlewares/invalidAttemptTracker.ts
+++ transaction/src/middlewares/invalidAttemptTracker.ts
@@ -1,5 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 
+const invalidAttempts = new Map<string, number>();
+
 export const invalidAttemptTracker = (
   req: Request,
   res: Response,
