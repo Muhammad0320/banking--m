686fc825563d529d6130a9466232a6cf430b926a
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-29 10:15:14 +0100

    test: add accpunt builder func for alltests

--- transaction/src/routes/__test__/newDeposit.test.ts
+++ transaction/src/routes/__test__/newDeposit.test.ts
@@ -1,7 +1,27 @@
 import request from 'supertest';
 import { app } from '../../app';
+import { Account } from '../../model/account';
+import mongoose from 'mongoose';
+import { AccountCurrency, AccountStatus } from '@m0banking/common';
 
-it('returns a 401 for unauthetcated user ', async () => {
+const accountBuilder = async () =>
+  await Account.buildAccount({
+    currency: AccountCurrency.NGN,
+
+    pin: '1234',
+
+    userId: new mongoose.Types.ObjectId().toHexString(),
+
+    status: AccountStatus.Active,
+
+    id: new mongoose.Types.ObjectId().toHexString(),
+    balance: 0,
+    version: 0,
+    no: 83923939393,
+    _block: false
+  });
+
+it('returns a 401 for unauthitcated user ', async () => {
   await request(app)
     .post('/api/v1/txn/deposit')
     .send()
