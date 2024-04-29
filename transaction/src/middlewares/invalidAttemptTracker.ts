fd2457ab86331652876ab72bdf1a9efecb69aa16
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-29 06:46:22 +0100

    chore: send message from the middleware

--- transaction/src/middlewares/invalidAttemptTracker.ts
+++ transaction/src/middlewares/invalidAttemptTracker.ts
@@ -6,4 +6,20 @@ export const invalidAttemptTracker = (
   req: Request,
   res: Response,
   next: NextFunction
-) => {};
+) => {
+  const { accountId } = req.body;
+
+  if (invalidAttempts.has(accountId)) {
+    const attempt = invalidAttempts.get(accountId) ?? 1 + 1;
+
+    invalidAttempts.set(accountId, attempt);
+
+    if (attempt === 5) {
+      res.locals.block = true;
+    }
+  } else {
+    invalidAttempts.set(accountId, 1);
+  }
+
+  next();
+};
