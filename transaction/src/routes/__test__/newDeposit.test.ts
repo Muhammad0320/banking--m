e76b29a6a9ec658df9a5772717008bcd49f1693c
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-29 11:35:39 +0100

    test: add test that  publishes a TxnDepositCreatedPublisher event when everything is valid

--- transaction/src/routes/__test__/newDeposit.test.ts
+++ transaction/src/routes/__test__/newDeposit.test.ts
@@ -3,6 +3,7 @@ import { app } from '../../app';
 import { Account } from '../../model/account';
 import mongoose from 'mongoose';
 import { AccountCurrency, AccountStatus } from '@m0banking/common';
+import { natsWrapper } from '../../../natswrapper';
 
 const accountBuilder = async () =>
   await Account.buildAccount({
@@ -107,4 +108,22 @@ it('returns returns a 201 when everything is valid ', async () => {
 
   expect(data.amount).toEqual(1000);
   expect(account.balance).toEqual(1000);
+});
+
+it(' publishes a TxnDepositCreatedPublisher event when everything is valid ', async () => {
+  const account = await accountBuilder();
+
+  const {
+    body: { data }
+  } = await request(app)
+    .post('/api/v1/txn/deposit')
+    .set('Cookie', await global.signin(account.userId))
+    .send({
+      amount: 1000,
+      accountId: account.id,
+      pin: account.pin
+    })
+    .expect(201);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
