ad793ccb42d071d706395cea3b97c8cdc0014414
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-30 09:31:16 +0100

    chore: update on message

--- account/src/events/listeners/TxnTransferCreatedListener.ts
+++ account/src/events/listeners/TxnTransferCreatedListener.ts
@@ -1,5 +1,35 @@
-import { Listener, TxnTransferCreatedEvent } from '@m0banking/common';
+import { Listener, Subjects, TxnTransferCreatedEvent } from '@m0banking/common';
+import { queueGroupName } from './queueGroupName';
+import { Message } from 'node-nats-streaming';
+import Account from '../../model/account';
 
 export class TxnTransferCreatedListener extends Listener<
   TxnTransferCreatedEvent
-> {}
+> {
+  readonly subject = Subjects.TxnTransferCreated;
+
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: TxnTransferCreatedEvent['data'], msg: Message) {
+    const account = await Account.findByLastVersionAndId(
+      data.account.id,
+      data.account.version
+    );
+
+    const beneficiary = await Account.findByLastVersionAndId(
+      data.beneficiary.id,
+      data.beneficiary.version
+    );
+
+    if (!account || !beneficiary)
+      throw new Error('Account or beneficiary not found');
+
+    await account.updateOne({ balace: data.account.balance }, { new: true });
+    await beneficiary.updateOne(
+      { balace: data.beneficiary.balance },
+      { new: true }
+    );
+
+    msg.ack();
+  }
+}
