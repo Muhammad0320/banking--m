6dc75efac04d2397fff170673bd51257eb72a0d8
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-30 09:17:08 +0100

    chore:  add txn deposited listenner

--- account/src/events/listeners/TxnDepositCretaedListener.ts
+++ account/src/events/listeners/TxnDepositCretaedListener.ts
@@ -1,1 +1,20 @@
-export class TxnDepositedPublisher {}
+import { Listener, Subjects, TxnDepositCreatedEvent } from '@m0banking/common';
+import { queueGroupName } from './queueGroupName';
+import { Message } from 'node-nats-streaming';
+import Account from '../../model/account';
+
+export class TxnDepositedListener extends Listener<TxnDepositCreatedEvent> {
+  readonly subject = Subjects.TxnDepositCreated;
+
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: TxnDepositCreatedEvent['data'], msg: Message) {
+    const account = await Account.findByLastVersionAndId(data.id, data.version);
+
+    if (!account) throw new Error('account not found');
+
+    await account.updateOne({ balace: data.account.balance }, { new: true });
+
+    msg.ack();
+  }
+}
