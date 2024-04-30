4efda051c07eb380ff1e73264aea2f64b30998d5
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-30 06:52:26 +0100

    chore; update code

--- transaction/src/routes/__test__/newTransfer.test.ts
+++ transaction/src/routes/__test__/newTransfer.test.ts
@@ -8,39 +8,29 @@ import {
   CryptoManager
 } from '@m0banking/common';
 
-const accountBuilder = async () =>
-  await Account.buildAccount({
+const accountBuilder = async (ben?: boolean) => {
+  const accountSender = new mongoose.Types.ObjectId().toHexString();
+  const accountBen = new mongoose.Types.ObjectId().toHexString();
+
+  const userSender = new mongoose.Types.ObjectId().toHexString();
+  const userBen = new mongoose.Types.ObjectId().toHexString();
+
+  return await Account.buildAccount({
     currency: AccountCurrency.NGN,
 
     pin: await CryptoManager.hash('1234'),
 
-    userId: new mongoose.Types.ObjectId().toHexString(),
+    userId: ben ? userBen : userSender,
 
     status: AccountStatus.Active,
 
-    id: new mongoose.Types.ObjectId().toHexString(),
+    id: ben ? accountBen : accountSender,
     balance: 0,
     version: 0,
     no: Math.floor(83923939393 * Math.random() * 1.5),
     _block: false
   });
-
-const benAccountBuilder = async () =>
-  await Account.buildAccount({
-    currency: AccountCurrency.NGN,
-
-    pin: await CryptoManager.hash('1234'),
-
-    userId: new mongoose.Types.ObjectId().toHexString(),
-
-    status: AccountStatus.Active,
-
-    id: new mongoose.Types.ObjectId().toHexString(),
-    balance: 0,
-    version: 0,
-    no: Math.floor(83923939390 * Math.random() * 1.5),
-    _block: false
-  });
+};
 
 it('returns a 401, for unautheticated user', async () => {
   await request(app)
@@ -107,8 +97,6 @@ it('returns a 400 for invalid ids: account & beneficiary', async () => {
 it('returns a  400 for invalid pin', async () => {
   const account = await accountBuilder();
 
-  const beneficiaryAccount = await benAccountBuilder();
-
   await request(app)
     .post('/api/v1/txn/transfer')
     .set('Cookie', await global.signin(account.userId))
@@ -116,6 +104,21 @@ it('returns a  400 for invalid pin', async () => {
       amount: 100,
       accountId: account.id,
       pin: 1237,
+      beneficiaryId: beneficiaryAccount.id
+    })
+    .expect(400);
+});
+
+it('returns a 201 when everything is valid', async () => {
+  const account = await accountBuilder();
+
+  await request(app)
+    .post('/api/v1/txn/transfer')
+    .set('Cookie', await global.signin(account.userId))
+    .send({
+      amount: 100,
+      accountId: account.id,
+      pin: 1234,
       beneficiaryId: beneficiaryAccount.id
     })
     .expect(400);
