9d0edd3eafe7ac43aaba6bb3bca6aa6e9f411cfd
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-01 11:34:14 +0100

    chore:update txn transfer listenner

--- notification/src/events/listener/TxnTransferListener.ts
+++ notification/src/events/listener/TxnTransferListener.ts
@@ -1,3 +1,22 @@
-import { Listener, TxnTransferCreatedEvent } from '@m0banking/common';
+import { Listener, Subjects, TxnTransferCreatedEvent } from '@m0banking/common';
+import { queueGroupName } from './queueGropName';
+import { Message } from 'node-nats-streaming';
+import { Notification } from '../../model/notification';
 
-export class TxnTransferListener extends Listener<TxnTransferCreatedEvent> {}
+export class TxnTransferListener extends Listener<TxnTransferCreatedEvent> {
+  readonly subject = Subjects.TxnTransferCreated;
+
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: TxnTransferCreatedEvent['data'], msg: Message) {
+    const title = 'Debit Alert!!!';
+
+    const description = `Dear Customer, Your account successfully transferred {{shit}} to {{shit}}  .\n Your new balace is: ${data.account.balance}`;
+
+    await Notification.buildNotification({
+      title,
+      description,
+      userId: data.account.id
+    });
+  }
+}
