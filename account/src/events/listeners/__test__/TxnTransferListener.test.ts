1c078daeb842694a74d3ae620581dcfe7b4c8ed6
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-01 12:23:05 +0100

    chore: add txn transfer

---
+++ account/src/events/listeners/__test__/TxnTransferListener.test.ts
@@ -0,0 +1,69 @@
+import {
+  AccountCurrency,
+  TxnTransferCreatedEvent,
+  UserRole
+} from '@m0banking/common';
+import { natsWrapper } from '../../../natswrapper';
+
+import Account from '../../../model/account';
+import { User, UserDoc } from '../../../model/user';
+import mongoose from 'mongoose';
+import { AccountTier } from '../../../enums/AccountTier';
+import { AccountType } from '../../../enums/AccountTypeEnum';
+import { Message } from 'node-nats-streaming';
+import { TxnTransferCreatedListener } from '../TxnTransferCreatedListener';
+
+const userBuilder = async () =>
+  await User.buildUser({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    password: 'shitpassword',
+
+    email: 'shitman@gmail.com',
+    name: 'Lisan al-gaib',
+    role: UserRole.User,
+    version: 0
+  });
+
+const accountBuilder = async (user: UserDoc) =>
+  await Account.buildAccount({
+    pin: '1234',
+    pinConfirm: '1234',
+
+    user,
+    tier: AccountTier.Basic,
+    currency: AccountCurrency.USD,
+    type: AccountType.Current
+  });
+
+const setup = async () => {
+  const listener = new TxnTransferCreatedListener(natsWrapper.client);
+
+  const user = await userBuilder();
+  const account = await accountBuilder(user);
+
+  const userBen = await userBuilder();
+  const benAccount = await accountBuilder(userBen);
+
+  const data: TxnTransferCreatedEvent['data'] = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    account: {
+      id: account.id,
+      version: account.version,
+      balance: 5000
+    },
+
+    beneficiary: {
+      id: benAccount.id,
+      version: benAccount.version,
+      balance: 4000
+    }
+  };
+
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn()
+  };
+
+  return { listener, data, account, msg };
+};
