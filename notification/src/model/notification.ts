417002cc8fb9c10f0986b79c3b13226ab00be065
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-01 04:10:48 +0100

    chore: update notification

--- notification/src/model/notification.ts
+++ notification/src/model/notification.ts
@@ -15,28 +15,50 @@ type NotificationModel = mongoose.Model<NotificationDoc> & {
   buildNotification(attrs: NotificationAttrs): Promise<NotificationDoc>;
 };
 
-const notSchema = new mongoose.Schema({
-  userId: {
-    type: String,
-    required: true
-  },
+const notSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true
+    },
 
-  title: {
-    type: String,
-    required: true
-  },
+    title: {
+      type: String,
+      required: true
+    },
 
-  description: {
-    type: String,
-    required: true
-  },
+    description: {
+      type: String,
+      required: true
+    },
 
-  createdAt: {
-    type: Date,
-    default: new Date()
-  },
+    createdAt: {
+      type: Date,
+      default: new Date()
+    },
 
-  status: {
-    type: String
+    status: {
+      type: String
+    }
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+
+        delete ret._id;
+      }
+    }
   }
-});
+);
+
+notSchema.statics.buildNotification = async function(attrs: NotificationAttrs) {
+  return await Notification.create(attrs);
+};
+
+const Notification = mongoose.model<NotificationDoc, NotificationModel>(
+  'notification',
+  notSchema
+);
+
+export { Notification };
