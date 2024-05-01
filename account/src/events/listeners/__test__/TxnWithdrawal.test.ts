4cde9dfa60f07f51f45e33ce9d66112bd7ed9597
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-01 12:09:48 +0100

    chore: add txn withdrawal test setup

---
+++ account/src/events/listeners/__test__/TxnWithdrawal.test.ts
@@ -0,0 +1,53 @@
+import mongoose from 'mongoose';
+import { User } from '../../../model/user';
+import { natsWrapper } from '../../../natswrapper';
+import { TxnWithdrawalCreatedListener } from '../TxnWithdrawalCreatedListener';
+import {
+  AccountCurrency,
+  TxnWithdrawalCreatedEvent,
+  UserRole
+} from '@m0banking/common';
+import { AccountTier } from '../../../enums/AccountTier';
+import { AccountType } from '../../../enums/AccountTypeEnum';
+import Account from '../../../model/account';
+
+const setup = async () => {
+  const listener = new TxnWithdrawalCreatedListener(natsWrapper.client);
+
+  const user = await User.buildUser({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    password: 'shitpassword',
+
+    email: 'shitman@gmail.com',
+    name: 'Lisan al-gaib',
+    role: UserRole.User,
+    version: 0
+  });
+
+  const account = await Account.buildAccount({
+    pin: '1234',
+    pinConfirm: '1234',
+
+    user,
+    tier: AccountTier.Basic,
+    currency: AccountCurrency.USD,
+    type: AccountType.Current
+  });
+
+  const data: TxnWithdrawalCreatedEvent['data'] = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    account: {
+      id: account.id,
+      version: account.version,
+      balance: 50
+    }
+  };
+
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn()
+  };
+
+  return { listener, data, account, msg };
+};
