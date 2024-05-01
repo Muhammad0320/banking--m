27d6acc6e6d5efecfe6f7ec3d2da006e67b23682
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-01 07:08:49 +0100

    chore: update account unclocked litener

--- notification/src/events/listener/AccountUnblockedListener.ts
+++ notification/src/events/listener/AccountUnblockedListener.ts
@@ -1,5 +1,25 @@
 import { AccountUnBlockedEvent, Listener, Subjects } from '@m0banking/common';
+import { queueGroupName } from './queueGropName';
+import { Message } from 'node-nats-streaming';
+import { Notification } from '../../model/notification';
 
 export class AccountUnblockedListener extends Listener<AccountUnBlockedEvent> {
   readonly subject = Subjects.AccountUnblocked;
+
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: AccountUnBlockedEvent['data'], msg: Message) {
+    const title = 'Account Unblocked';
+
+    const description =
+      "Dear customer, Your Account has been successfully unblocked, now it's safe to transact";
+
+    await Notification.buildNotification({
+      title,
+      description,
+      userId: data.user.id
+    });
+
+    msg.ack();
+  }
 }
