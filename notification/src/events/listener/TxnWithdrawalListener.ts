e99466d4e1efe41631c36269a71c894d3d6b3b61
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-01 11:26:47 +0100

    chore: update txn withdrawal listener

--- notification/src/events/listener/TxnWithdrawalListener.ts
+++ notification/src/events/listener/TxnWithdrawalListener.ts
@@ -1,14 +1,18 @@
-import { Listener, Subjects, TxnDepositCreatedEvent } from '@m0banking/common';
+import {
+  Listener,
+  Subjects,
+  TxnWithdrawalCreatedEvent
+} from '@m0banking/common';
 import { queueGroupName } from './queueGropName';
 import { Message } from 'node-nats-streaming';
 import { Notification } from '../../model/notification';
 
-export class TxnDeositListener extends Listener<TxnDepositCreatedEvent> {
-  readonly subject = Subjects.TxnDepositCreated;
+export class TxnwithdrawalListener extends Listener<TxnWithdrawalCreatedEvent> {
+  readonly subject = Subjects.TxnWithdrawalCreated;
 
   queueGroupName = queueGroupName;
 
-  async onMessage(data: TxnDepositCreatedEvent['data'], msg: Message) {
+  async onMessage(data: TxnWithdrawalCreatedEvent['data'], msg: Message) {
     const title = 'Debit Alert!!!';
 
     const description = `Dear Customer, Your account has been debited with shit amout. \n Your new balace is: ${data.account.balance}`;
