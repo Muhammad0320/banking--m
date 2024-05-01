c57d40b16478efc0e968677d6942fc6f27e80898
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-01 06:59:43 +0100

    chore: AccountBlockedListener

--- notification/src/events/listener/AccountUpdatedListener.ts
+++ notification/src/events/listener/AccountUpdatedListener.ts
@@ -1,3 +1,25 @@
-import { AccountBlockedEvent, Listener } from '@m0banking/common';
+import { AccountBlockedEvent, Listener, Subjects } from '@m0banking/common';
+import { queueGroupName } from './queueGropName';
+import { Message } from 'node-nats-streaming';
+import { Notification } from '../../model/notification';
 
-export class AccountBlockedListener extends Listener<AccountBlockedEvent> {}
+export class AccountBlockedListener extends Listener<AccountBlockedEvent> {
+  readonly subject = Subjects.AccountBlocked;
+
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: AccountBlockedEvent['data'], msg: Message) {
+    const title = 'Account Blocked!';
+
+    const description =
+      'Dear Customer, Your account has been blocked due to a kind of fraud detection';
+
+    await Notification.buildNotification({
+      title,
+      description,
+      userId: data.user.id
+    });
+
+    msg.ack();
+  }
+}
