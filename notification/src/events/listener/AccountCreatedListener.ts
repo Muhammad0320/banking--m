9ee8e2a17a3e842d980f81847f6ea3c12ef73ffb
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-01 06:49:47 +0100

    chore: update the account createdlistener

--- notification/src/events/listener/AccountCreatedListener.ts
+++ notification/src/events/listener/AccountCreatedListener.ts
@@ -1,1 +1,30 @@
-export class AccountCreatedListener {}
+import { Listener, AccountCreatedEvent, Subjects } from '@m0banking/common';
+import { queueGroupName } from './queueGropName';
+import { Message } from 'node-nats-streaming';
+import { Notification } from '../../model/notification';
+
+// Add name and type of the account to the user;
+
+export class AccountCreatedListener extends Listener<AccountCreatedEvent> {
+  readonly subject = Subjects.AccountCreated;
+
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: AccountCreatedEvent['data'], msg: Message) {
+    const title = 'New Account Created';
+    const description =
+      `Hello ${data.userId}, a new account has been created for you with the following details:\n` +
+      `- Account Number: ${data.no}\n` +
+      // `- Account Type: ${data}\n` +
+      `- Balance: ${data.balance}\n\n` +
+      'Thank you for banking with us!';
+
+    await Notification.buildNotification({
+      title,
+      description,
+      userId: data.userId
+    });
+
+    msg.ack();
+  }
+}
