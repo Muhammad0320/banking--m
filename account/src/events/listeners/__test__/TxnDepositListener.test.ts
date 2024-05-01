20175f8ddb3729d4ca2b0725c2f42089f6d20971
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-01 11:55:23 +0100

    chore: update txn deposiut listenenr

--- account/src/events/listeners/__test__/TxnDepositListener.test.ts
+++ account/src/events/listeners/__test__/TxnDepositListener.test.ts
@@ -1,58 +1,48 @@
-import { AccountCurrency, TxnDepositCreatedEvent, UserRole } from "@m0banking/common"
-import { natsWrapper } from "../../../natswrapper"
-import { TxnDepositedListener } from "../TxnDepositCretaedListener"
-import Account from "../../../model/account"
-import { User } from "../../../model/user"
-import mongoose from "mongoose"
-import { AccountTier } from "../../../enums/AccountTier"
-import { AccountType } from "../../../enums/AccountTypeEnum"
-
-
+import {
+  AccountCurrency,
+  TxnDepositCreatedEvent,
+  UserRole
+} from '@m0banking/common';
+import { natsWrapper } from '../../../natswrapper';
+import { TxnDepositedListener } from '../TxnDepositCretaedListener';
+import Account from '../../../model/account';
+import { User } from '../../../model/user';
+import mongoose from 'mongoose';
+import { AccountTier } from '../../../enums/AccountTier';
+import { AccountType } from '../../../enums/AccountTypeEnum';
 
 const setup = async () => {
+  const listener = new TxnDepositedListener(natsWrapper.client);
 
-
-    const listener = new TxnDepositedListener(natsWrapper.client)
-
-    const user = await User.buildUser({ id: new mongoose.Types.ObjectId().toHexString(), password: 'shitpassword', 
+  const user = await User.buildUser({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    password: 'shitpassword',
 
     email: 'shitman@gmail.com',
-    name: "Lisan al-gaib",
+    name: 'Lisan al-gaib',
     role: UserRole.User,
     version: 0
+  });
 
+  const account = await Account.buildAccount({
+    pin: '1234',
+    pinConfirm: '1234',
 
-     })
+    user,
+    tier: AccountTier.Basic,
+    currency: AccountCurrency.USD,
+    type: AccountType.Current
+  });
 
-    const account = await Account.buildAccount({
-
-        pin: '1234',
-pinConfirm: '1234',
-
-user,
-tier: AccountTier.Basic,
-currency: AccountCurrency.USD,
-type: AccountType.Current
-
-    })
-
-    const data: TxnDepositCreatedEvent['data'] = {
-
-        id: new mongoose.Types.ObjectId().toHexString(),
-        version: 0,
-        account: {
-            id: account.id,
-            version: account.version,
-            balance: account.balace,
-        }
-
+  const data: TxnDepositCreatedEvent['data'] = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    account: {
+      id: account.id,
+      version: account.version,
+      balance: account.balace
     }
+  };
 
-
-
-    return { listener, data, account }
-
-
-}
-
-
+  return { listener, data, account };
+};
