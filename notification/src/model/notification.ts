5fda71abf941bec6c456700f5d354c56230bba2c
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-01 04:04:01 +0100

    chore: update notitdication ts

--- notification/src/model/notification.ts
+++ notification/src/model/notification.ts
@@ -4,12 +4,39 @@ type NotificationAttrs = {
   userId: string;
   title: string;
   description: string;
-  timeStamp: Date;
-  read: string; // enum
 };
 
-type NotificationDoc = mongoose.Document<NotificationAttrs>;
+type NotificationDoc = mongoose.Document<NotificationAttrs> & {
+  createdAt: Date;
+  status: string; // enum
+};
 
 type NotificationModel = mongoose.Model<NotificationDoc> & {
   buildNotification(attrs: NotificationAttrs): Promise<NotificationDoc>;
 };
+
+const notSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true
+  },
+
+  title: {
+    type: String,
+    required: true
+  },
+
+  description: {
+    type: String,
+    required: true
+  },
+
+  createdAt: {
+    type: Date,
+    default: new Date()
+  },
+
+  status: {
+    type: String
+  }
+});
