39997d65e81eb2ea8dc5d8a915edc7356fc9c6bf
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-01 04:33:42 +0100

    chore: update all ts

--- notification/src/routes/all.ts
+++ notification/src/routes/all.ts
@@ -1,17 +1,15 @@
 import express, { Request, Response } from 'express';
-
+import { requireAuth } from '@m0banking/common';
+import { Notification } from '../model/notification';
 
 const router = express.Router();
 
-router.get('/' ,async (req: Request, res: Response) => {
+router.get('/', requireAuth, async (req: Request, res: Response) => {
+  const notification = await Notification.findOne({
+    userId: req.currentUser.id
+  });
 
-
-    
-
-} )
-
-
+  res.status(200).json({ status: 'success', data: notification });
+});
 
 export { router as GetAllNotification };
-
-
