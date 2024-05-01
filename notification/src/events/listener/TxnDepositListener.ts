e8102b2e740b36d0b017eb0b4607e53ff797262f
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-01 07:28:24 +0100

    chore: update txn depoisit listenner

---
+++ notification/src/events/listener/TxnDepositListener.ts
@@ -0,0 +1,24 @@
+import { Listener, Subjects, TxnDepositCreatedEvent } from '@m0banking/common';
+import { queueGroupName } from './queueGropName';
+import { Message } from 'node-nats-streaming';
+import { Notification } from '../../model/notification';
+
+export class TxnDeositListener extends Listener<TxnDepositCreatedEvent> {
+  readonly subject = Subjects.TxnDepositCreated;
+
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: TxnDepositCreatedEvent['data'], msg: Message) {
+    const title = 'Credit Alert!!!';
+
+    const description = `Dear Customer, Your account has been credited with shit amout. \n Your new balace is: ${data.account.balance}`;
+
+    await Notification.buildNotification({
+      title,
+      description,
+      userId: 'shit id'
+    });
+
+    msg.ack();
+  }
+}
