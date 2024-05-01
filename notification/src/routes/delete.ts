028e628e3ecab85dc94dcb10718737975b1dc6e0
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-01 04:49:19 +0100

    chore: update delete handler

--- notification/src/routes/delete.ts
+++ notification/src/routes/delete.ts
@@ -1,5 +1,6 @@
 import { paramsChecker, requireAuth } from '@m0banking/common';
 import express, { Request, Response } from 'express';
+import { Notification } from '../model/notification';
 
 const router = express.Router();
 
@@ -7,7 +8,17 @@ router.delete(
   '/',
   requireAuth,
   paramsChecker('id'),
-  async (req: Request, res: Response) => {}
+  async (req: Request, res: Response) => {
+    const notification = await Notification.findOneAndDelete({
+      userId: req.currentUser.id,
+      id: req.params.id
+    });
+
+    res.status(204).json({
+      status: 'success',
+      data: notification
+    });
+  }
 );
 
 export { router as deleteAllNotification };
