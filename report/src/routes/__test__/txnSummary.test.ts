2009e7fe66a694a32bfb8fe8efe9075de279bc0f
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-07 05:10:01 +0100

    chore:  update txn summary

--- report/src/routes/__test__/txnSummary.test.ts
+++ report/src/routes/__test__/txnSummary.test.ts
@@ -1,5 +1,50 @@
+import mongoose from 'mongoose';
 import { app } from '../../app';
 import request from 'supertest';
+import {
+  AccountCurrency,
+  AccountStatus,
+  AccountTier,
+  AccountType,
+  CryptoManager,
+  TxnTypeEnum
+} from '@m0banking/common';
+import { Account } from '../../models/account';
+import { Txn } from '../../models/transaction';
+
+const accountBuilder = async (userId: string) => {
+  return await Account.buildAccount({
+    currency: AccountCurrency.NGN,
+
+    pin: await CryptoManager.hash('1234'),
+
+    userId,
+
+    status: AccountStatus.Active,
+
+    id: new mongoose.Types.ObjectId().toHexString(),
+    balance: 0,
+    version: 0,
+    no: Math.floor(83923939393 * Math.random() * 1.5),
+    _block: false,
+    tier: AccountTier.Basic,
+    type: AccountType.Current
+  });
+};
+
+const txnBuilder = async (
+  amount: number,
+  type: TxnTypeEnum,
+  userId: string
+) => {
+  const account = await accountBuilder(userId);
+
+  return await Txn.buildTxn({
+    account,
+    amount,
+    type
+  });
+};
 
 it('returns a 401, for unauthorized user', async () => {
   await request(app)
@@ -17,6 +62,15 @@ it('returns status other than 401, to show that routes exists', async () => {
 });
 
 it('returns a 200 status code when for valid user', async () => {
+  const userId = new mongoose.Types.ObjectId().toHexString();
+
+  txnBuilder(5000, TxnTypeEnum.Deposit, userId);
+  txnBuilder(10000, TxnTypeEnum.Deposit, userId);
+  txnBuilder(1000, TxnTypeEnum.Deposit, userId);
+  txnBuilder(1000, TxnTypeEnum.Withdrawal, userId);
+  txnBuilder(900, TxnTypeEnum.Withdrawal, userId);
+  txnBuilder(900, TxnTypeEnum.Transfer, userId);
+
   const {
     body: { data }
   } = await request(app)
