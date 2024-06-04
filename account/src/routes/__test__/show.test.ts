92dd370aa2dd8f10a31d34a5f3f833e44d0a300e
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-06-04 08:23:10 +0100

    chore:  add another gueard

--- account/src/routes/__test__/show.test.ts
+++ account/src/routes/__test__/show.test.ts
@@ -37,26 +37,46 @@ it('returns a 400, if user passes invalid mongoose id', async () => {
 });
 
 it('returns a 403, if user tried to check other users account', async () => {
-  const {
-    body: { data }
-  } = await request(app)
-    .post('/api/v1/account')
-    .set(
-      'Cookie',
-      await global.signin(new mongoose.Types.ObjectId().toHexString())
-    )
-    .send({
-      currency: AccountCurrency.NGN,
-      tier: AccountTier.Basic,
-      pin: 1234,
-      pinConfirm: 1234
-    })
-    .expect(201);
+  // const {
+  //   body: { data }
+  // } = await request(app)
+  //   .post('/api/v1/account')
+  //   .set(
+  //     'Cookie',
+  //     await global.signin(new mongoose.Types.ObjectId().toHexString())
+  //   )
+  //   .send({
+  //     currency: AccountCurrency.NGN,
+  //     tier: AccountTier.Basic,
+  //     pin: 1234,
+  //     pinConfirm: 1234
+  //   })
+  //   .expect(201);
 
-  console.log(data, 'from the test');
+  // console.log(data, 'from the test');
+
+  const user = await User.buildUser({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    name: 'Lisan al Gaib',
+    password: 'shitword',
+    email: 'shit@shit.com',
+    role: UserRole.User,
+    version: 0
+  });
+
+  const account = await Account.buildAccount({
+    currency: AccountCurrency.NGN,
+    tier: AccountTier.Basic,
+    pin: '1234',
+    pinConfirm: '1234',
+    type: AccountType.Savings,
+    user
+  });
+
+  if (!!!(await Account.findById(account.id))) throw new Error('shittt');
 
   await request(app)
-    .get('/api/v1/account/' + data.id)
+    .get('/api/v1/account/' + account.id)
     .set('Cookie', await global.signin())
     .send()
     .expect(403);
