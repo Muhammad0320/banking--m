1c7a7fa1febf857cb1965b4e956fe526d3557d79
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-06-06 18:32:33 +0100

    chore: update test

--- user/src/routes/__test__/update.test.ts
+++ user/src/routes/__test__/update.test.ts
@@ -4,16 +4,7 @@ import request from 'supertest';
 import User from '../../model/user';
 import { natsWrapper } from '../../natswrapper';
 
-it('returns error order that 400, if route exists', async () => {
-  const response = await request(app)
-    .patch('/api/v1/user/' + new mongoose.Types.ObjectId().toString('hex'))
-    .set('Cookie', await global.signin())
-    .send();
-
-  expect(response.statusCode).not.toEqual(404);
-});
-
-it('returs a 400 on invalid id', async () => {
+it('returs a 404 on invalid id', async () => {
   await request(app)
     .patch('/api/v1/user/' + new mongoose.Types.ObjectId().toString('hex'))
     .set('Cookie', await global.signin())
