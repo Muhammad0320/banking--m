a7d20f86886a26c78d54b6c2bec96096bf0eeda4
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-06-09 12:25:26 +0100

    feat: add card schema

---
+++ card/src/model/card.ts
@@ -0,0 +1,78 @@
+import mongoose, { Mongoose } from 'mongoose';
+
+import { CardType } from '../enums/CardType';
+import { AccountDoc } from './account';
+import { Info, Settings, User } from '../types/CardFieldTypes';
+import { CardNetwork } from '../enums/CardNewtwork';
+import { CardStatus } from '../enums/CardStatus';
+
+// type CardAttrs = {
+
+//     billingAddress: string,
+//     networkType: CardNetworkType,
+//     type: CardType
+
+// };
+
+type CardDoc = mongoose.Document & {
+  account: AccountDoc;
+  user: User;
+  settings: Settings;
+  info: Info;
+  version: number;
+};
+
+type CardModel = mongoose.Model<CardDoc> & {
+  findByLastVersionAndId(id: string, version: number): Promise<CardDoc | null>;
+};
+
+const cardSchema = new mongoose.Schema<CardDoc, CardModel>({
+  account: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Account'
+  },
+
+  user: {
+    id: String,
+    name: String
+  },
+
+  settings: {
+    dailyLimit: Number,
+    weeklyLimit: Number,
+    monthlyLimit: Number
+  },
+
+  info: {
+    no: {
+      type: String,
+      required: true,
+      unique: true
+    },
+
+    network: {
+      type: String,
+      enum: Object.values(CardNetwork)
+    },
+
+    type: {
+      type: String,
+      enum: Object.values(CardType)
+    },
+
+    cvv: String,
+    expiryDate: Date,
+    issueDate: Date,
+    billingAddress: {
+      type: String,
+      required: true,
+      maxlength: 400,
+      minlength: 20
+    },
+
+    status: {
+      type: String,
+      enum: Object.values(CardStatus)
+    }
+  }
+});
