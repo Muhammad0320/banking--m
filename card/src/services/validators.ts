79a7f090fd6f876603cee1c3db178098efee5509
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-06-10 14:53:04 +0100

    chore: update validators

--- card/src/services/validators.ts
+++ card/src/services/validators.ts
@@ -33,3 +33,27 @@ export const typeValidator = () =>
       Object.values(CardType).includes(input as CardType)
     )
     .withMessage('Please provide a valid Card type');
+
+export const dailyLimits = () =>
+  body('daily')
+    .trim()
+    .notEmpty()
+    .isNumeric()
+    .isLength({ min: 1, max: 500 })
+    .withMessage('Monthly update should not be greater than 500');
+
+export const weeklyLimits = () =>
+  body('weekly')
+    .trim()
+    .notEmpty()
+    .isNumeric()
+    .isLength({ min: 1, max: 5000 })
+    .withMessage('Monthly update should not be greater than 5000');
+
+export const monthlyLimits = () =>
+  body('month')
+    .trim()
+    .notEmpty()
+    .isNumeric()
+    .isLength({ min: 1, max: 500000 })
+    .withMessage('Monthly update should not be greater than 50000');
