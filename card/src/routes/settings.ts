f54d72e56949fa0267e79e4d72f536ef28cc2182
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-06-10 15:45:21 +0100

    chore:  update settings

--- card/src/routes/settings.ts
+++ card/src/routes/settings.ts
@@ -1,4 +1,5 @@
 import {
+  NotFound,
   paramsChecker,
   requestValidator,
   requireAuth
@@ -9,6 +10,7 @@ import {
   monthlyLimitsValidator,
   weeklyLimitsValidator
 } from '../services/validators';
+import { Card } from '../model/card';
 
 const router = express.Router();
 
@@ -18,5 +20,21 @@ router.patch(
   paramsChecker('id'),
   [dailyLimitsValidator, weeklyLimitsValidator, monthlyLimitsValidator],
   requestValidator,
-  async (req: Request, res: Response) => {}
+  async (req: Request, res: Response) => {
+    const { weekly, monthly, daily } = req.body;
+
+    const card = await Card.findById(req.params.id);
+
+    if (!card) throw new NotFound('Card not found error');
+
+    const updatedCard = await card.updateOne(
+      { settings: { weekly, monthly, daily } },
+      { new: true }
+    );
+
+    res.status(200).json({
+      status: 'success',
+      card: updatedCard
+    });
+  }
 );
