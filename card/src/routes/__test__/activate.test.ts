95df92a6c858f4231f51b3f6f2ce6cdfef3354df
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-06-11 20:05:43 +0100

    chore: returns a 200 when everyting is valid

--- card/src/routes/__test__/activate.test.ts
+++ card/src/routes/__test__/activate.test.ts
@@ -1,6 +1,9 @@
 import request from 'supertest';
 import { app } from '../../app';
 import mongoose from 'mongoose';
+import { CardNetwork } from '../../enums/CardNewtwork';
+import { CardType } from '../../enums/CardType';
+import { accountBuilder } from '../../test/builders';
 
 it(' returns a 401 for unauthenticated requests ', async () => {
   await request(app)
@@ -22,6 +25,29 @@ it('retuns a 404 if the id does not match any existing card', async () => {
 
   await request(app)
     .patch(`/${id}/activate`)
+    .set('Cookie', await global.signin())
+    .send()
+    .expect(404);
+});
+
+it('returns a 200 when everyting is valid ', async () => {
+  const account = await accountBuilder();
+
+  const {
+    body: { data }
+  } = await request(app)
+    .post('/api/v1/card')
+    .set('Cookie', await global.signin(account.user.id))
+    .send({
+      accountId: account.id,
+      billingAddress: 'G50 Balogun gambari compd',
+      networkType: CardNetwork.Visa,
+      type: CardType.Credit
+    })
+    .expect(201);
+
+  await request(app)
+    .patch(`/${data.id}/activate`)
     .set('Cookie', await global.signin())
     .send()
     .expect(404);
