d8c54c8738aeef6faf74b57d2713a58343c5def0
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-06-11 20:10:02 +0100

    chore: add test that  returns a 401 for unauthenticated requests

---
+++ card/src/routes/__test__/settings.test.ts
@@ -0,0 +1,17 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it(' returns a 401 for unauthenticated requests ', async () => {
+  await request(app)
+    .patch('/shitid/activate')
+    .send()
+    .expect(401);
+});
+
+it('returns a 400 for invalid card ', async () => {
+  await request(app)
+    .patch('/shitid/activate')
+    .set('Cookie', await global.signin())
+    .send()
+    .expect(401);
+});
