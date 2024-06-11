dae8d215fa79d45b735f04f0d7e386e8f3c1c587
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-06-11 06:48:59 +0100

    chore:  add test that retuns a 400, for invalid accountId

--- card/src/routes/__test__/new.test.ts
+++ card/src/routes/__test__/new.test.ts
@@ -7,3 +7,13 @@ it('returns a 401 for unauthenticated route access', async () => {
     .send({})
     .expect(401);
 });
+
+it('retuns a 400, for invalid accountId ', async () => {
+  await request(app)
+    .post('/api/v1/card')
+    .set('Cookie', await global.signin())
+    .send({})
+    .expect(401);
+});
+
+it('returns a 404 on valid but not matched accountId', () => {});
