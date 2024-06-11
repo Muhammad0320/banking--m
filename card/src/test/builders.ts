7f65dcae4c47863e2dacb81ec731a8521b21c64e
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-06-11 07:09:00 +0100

    chore: update builder ts

--- card/src/test/builders.ts
+++ card/src/test/builders.ts
@@ -1,28 +1,28 @@
-import mongoose from "mongoose";
-import { Account } from "../model/account";
-import { AccountCurrency, AccountStatus, AccountTier, AccountType } from "@m0banking/common";
+import mongoose from 'mongoose';
+import { Account } from '../model/account';
+import {
+  AccountCurrency,
+  AccountStatus,
+  AccountTier,
+  AccountType
+} from '@m0banking/common';
 
+export const accountBuilder = async (accId: string, userId: string) => {
+  return await Account.buildAccount({
+    id: accId || new mongoose.Types.ObjectId().toHexString(),
+    no: 2349043000,
+    user: {
+      name: 'Muadib',
+      id: userId || new mongoose.Types.ObjectId().toHexString()
+    },
+    balance: 9999,
+    pin: '1234',
+    tier: AccountTier.Private,
+    type: AccountType.Current,
+    status: AccountStatus.Active,
+    currency: AccountCurrency.USD,
 
-
-export const accountBuilder = () => {
-
-
-    return new Account.buildAccount({   id: new mongoose.Types.ObjectId().toHexString(),
-        no: 2349043000,
-        user: { name: "Muadib",   id:    new mongoose.Types.ObjectId().toHexString() },
-        balance: 9999,
-        pin: '1234'
-tier: AccountTier.Private,
-type: AccountType.Current,
-status: AccountStatus.Active,
-currency: AccountCurrency.USD,
-
-version: 0,
-_block: false
-
-    
-    })
-
-}
-
-
+    version: 0,
+    _block: false
+  });
+};
