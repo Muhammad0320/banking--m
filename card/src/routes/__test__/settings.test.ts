7c7f0fcf9ceaf430744f6c9cd543a7ed435a03d9
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-06-11 20:15:25 +0100

    chore: add settings that returns a 400 on invalid dailyLimit

--- card/src/routes/__test__/settings.test.ts
+++ card/src/routes/__test__/settings.test.ts
@@ -1,17 +1,42 @@
 import request from 'supertest';
 import { app } from '../../app';
+import mongoose from 'mongoose';
 
 it(' returns a 401 for unauthenticated requests ', async () => {
   await request(app)
-    .patch('/shitid/activate')
+    .patch('/shitid/settings')
     .send()
     .expect(401);
 });
 
 it('returns a 400 for invalid card ', async () => {
   await request(app)
-    .patch('/shitid/activate')
+    .patch('/shitid/settings')
     .set('Cookie', await global.signin())
     .send()
-    .expect(401);
+    .expect(400);
+});
+
+it('returns a 400 on invalid dailyLimit', async () => {
+  const id = new mongoose.Types.ObjectId().toHexString();
+
+  await request(app)
+    .patch(`/${id}/settings`)
+    .set('Cookie', await global.signin())
+    .send({
+      daily: 500,
+      weekly: 50,
+      monthly: 5000
+    })
+    .expect(400);
+
+  await request(app)
+    .patch(`/${id}/settings`)
+    .set('Cookie', await global.signin())
+    .send({
+      daily: 500,
+      weekly: 0,
+      monthly: 5000
+    })
+    .expect(400);
 });
