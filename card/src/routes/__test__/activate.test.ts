5c81ba2fb45593fa100f6a1de58dd51ea0a7d6c7
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-06-11 19:56:28 +0100

    chore:  active test

--- card/src/routes/__test__/activate.test.ts
+++ card/src/routes/__test__/activate.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import { app } from '../../app';
+import mongoose from 'mongoose';
 
 it(' returns a 401 for unauthenticated requests ', async () => {
   await request(app)
@@ -8,15 +9,20 @@ it(' returns a 401 for unauthenticated requests ', async () => {
     .expect(401);
 });
 
-
-
 it('returns a 400 for invalid card ', async () => {
+  await request(app)
+    .patch('/shitid/activate')
+    .set('Cookie', await global.signin())
+    .send()
+    .expect(401);
+});
 
+it('retuns a 404 if the id does not match any existing card', async () => {
+  const id = new mongoose.Types.ObjectId().toHexString();
 
   await request(app)
-  .patch('/shitid/activate')
-  .set('Cookie', await global.signin())
-  .send()
-  .expect(401);
-
-})
+    .patch(`/${id}/activate`)
+    .set('Cookie', await global.signin())
+    .send()
+    .expect(404);
+});
