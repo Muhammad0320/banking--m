c2fa131a353444fab02c3a6211548f91d87740d1
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-06-11 12:52:22 +0100

    chore: add test that returns a 404 if the provided id is nit matched w/ any card

--- card/src/routes/__test__/block.test.ts
+++ card/src/routes/__test__/block.test.ts
@@ -1,9 +1,20 @@
 import request from 'supertest';
 import { app } from '../../app';
+import mongoose from 'mongoose';
 
-it('it returns a 401, for unautheticated user', async () => {
+it('it returns a 401 for unautheticated user', async () => {
   await request(app)
     .patch('/shit_id/block')
     .send()
     .expect(401);
+});
+
+it('returns a 404 if the provided id is nit matched w/ any card ', async () => {
+  const cardId = new mongoose.Types.ObjectId().toHexString();
+
+  await request(app)
+    .patch(`/${cardId}/block`)
+    .set('Cookie', await global.signin())
+    .send()
+    .expect(404);
 });
