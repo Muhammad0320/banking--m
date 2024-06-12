59006c1e96560ad44c460a34bcbdfc9580468299
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-06-12 10:39:01 +0100

    chore: update card model in txn

---
+++ transaction/src/model/card.ts
@@ -0,0 +1,114 @@
+import mongoose from 'mongoose';
+import { AccountDoc } from './account';
+import {
+  CardNetwork,
+  CardStatus,
+  CardType,
+  Info,
+  Settings,
+  User
+} from '@m0banking/common';
+
+type CardDoc = mongoose.Document & {
+  account: AccountDoc;
+  user: User;
+  settings: Settings;
+  info: Info;
+  version: number;
+};
+
+type CardModel = mongoose.Model<CardDoc> & {
+  findByLastVersionAndId(id: string, version: number): Promise<CardDoc | null>;
+};
+
+const cardSchema = new mongoose.Schema<CardDoc, CardModel>({
+  account: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Account'
+  },
+
+  user: {
+    id: String,
+    name: String
+  },
+
+  settings: {
+    dailyLimit: {
+      type: Number,
+      default: 500
+    },
+    weeklyLimit: {
+      type: Number,
+      default: 500
+    },
+    monthlyLimit: {
+      type: Number,
+      default: 5000
+    }
+  },
+
+  info: {
+    no: {
+      type: String,
+      required: true,
+      unique: true
+    },
+
+    network: {
+      type: String,
+      enum: Object.values(CardNetwork)
+    },
+
+    type: {
+      type: String,
+      enum: Object.values(CardType)
+    },
+
+    cvv: String,
+    expiryDate: {
+      type: Date
+    },
+    issueDate: {
+      type: Date,
+      default: new Date()
+    },
+    billingAddress: {
+      type: String,
+      required: true,
+      maxlength: 400,
+      minlength: 20
+    },
+
+    maxCredit: {
+      type: Number,
+      default: 50
+    },
+
+    status: {
+      type: String,
+      enum: Object.values(CardStatus),
+      default: CardStatus.Inactive
+    }
+  }
+});
+
+cardSchema.pre('save', async function(next) {
+  if (this.isModified() && this.info.type === CardType.Debit) {
+    this.info.maxCredit = undefined;
+  }
+
+  next();
+});
+
+cardSchema.statics.findByLastVersionAndId = async function(
+  id: string,
+  version: number
+) {
+  const __v = version - 1;
+
+  return await Card.findOne({ _id: id, version: __v });
+};
+
+const Card = mongoose.model<CardDoc, CardModel>('Card', cardSchema);
+
+export { Card };
