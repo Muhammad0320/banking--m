f097bcab823782ef61f90f830cc2b0ebb4daf6fc
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-06-12 10:32:34 +0100

    chore: add card field types

---
+++ common/src/types/CardFieldTypes.ts
@@ -0,0 +1,27 @@
+import { CardNetwork } from "../enums/CardNewtwork";
+import { CardStatus } from "../enums/CardStatus";
+import { CardType } from "../enums/CardType";
+
+export interface User {
+  id: string;
+  name: string;
+}
+
+export interface Settings {
+  dailyLimit: number;
+  weeklyLimit: number;
+  monthlyLimit: number;
+}
+
+export interface Info {
+  no: string;
+  network: CardNetwork;
+  status: CardStatus;
+  type: CardType;
+  cvv: string;
+  expiryDate: Date;
+  issueDate: Date;
+
+  billingAddress: string;
+  maxCredit: number | undefined;
+}
