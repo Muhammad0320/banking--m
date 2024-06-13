e6da5d54484145eb61eede27b2641a4d5647644c
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-06-13 18:43:10 +0100

    test: add card txn test

--- transaction/src/routes/__test__/cardTxn.test.ts
+++ transaction/src/routes/__test__/cardTxn.test.ts
@@ -521,3 +521,38 @@ it('returns a 400 for expired or inactive card', async () => {
     })
     .expect(400);
 });
+
+it('returns a 400 for insufficient fund', async () => {
+  const {
+    card: { hashed: hashedNo, unhashed: unhashedNo },
+    cvv: { hashed: hashedcvv, unhashed: unhashedcvv }
+  } = hashingWork();
+
+  const cardData: cardDataType = {
+    no: hashedNo,
+    cvv: hashedcvv,
+    billingAddress: 'G50, Balogun Gambari compound'
+  };
+
+  const account = await accountBuilder(AccountStatus.Active, 50);
+  const beneficiaryAccount = await accountBuilder(AccountStatus.Blocked, 50);
+
+  const card = await cardBuilder(account, cardData);
+
+  await request(app)
+    .post('/api/v1/txn/card')
+    .set('Cookie', await global.signin(account.user.id))
+    .send({
+      no: +unhashedNo,
+      cvv: +unhashedcvv,
+      expMonth: 1,
+      expYear: 2025,
+      cardName: 'Muhammad Awwal',
+      billingAddress: card.info.billingAddress,
+      amount: 500,
+      reason: 'Shit',
+      beneficiary: beneficiaryAccount.id,
+      account: account.id
+    })
+    .expect(400);
+});
